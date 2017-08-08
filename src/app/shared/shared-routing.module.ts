8ca860d5a2185d3ad97a30fb942c3d039cd7a0d9
Johannes Hiemer <jhiemer@evoila.de>
2017-08-08 07:59:08 +0200

    feat(file-endpoint): Implemented the file endpoint component

--- src/app/shared/shared-routing.module.ts
+++ src/app/shared/shared-routing.module.ts
@@ -5,6 +5,7 @@ import { BackupPlanComponent } from './backup/backup-plan/backup-plan.component'
 import { BackupJobComponent } from './backup/backup-job/backup-job.component';
 import { RestoreJobComponent } from './backup/restore-job/restore-job.component';
 import { BackupJobDetailsComponent } from './backup/backup-job/backup-job-details/backup-job-details.component';
+import { FileEndpointComponent } from './backup/file-endpoint/file-endpoint.component';
 
 export const ROUTES = [
   {
@@ -12,20 +13,28 @@ export const ROUTES = [
     component: BackupComponent,
   },
   {
-    path: 'backup/create-plan',
+    path: 'backup/plan',
     component: BackupPlanComponent
-  },
-  {
-    path: 'backup/create-job',
-    component: BackupJobComponent
   },
   {
     path: 'backup/plan/:planId',
     component: BackupPlanComponent
   },
-    {
+  {
+    path: 'backup/job',
+    component: BackupJobComponent
+  },
+  {
     path: 'backup/job/:jobId',
     component: BackupJobDetailsComponent
+  },
+  {
+    path: 'backup/file-endpoint',
+    component: FileEndpointComponent
+  },
+  {
+    path: 'backup/file-endpoint/:jobId',
+    component: FileEndpointComponent
   },
   {
     path: 'backup/create-restore',
