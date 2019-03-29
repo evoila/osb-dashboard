5b808fc7221f8c28cf5f343a3be6ee31afed1302
Michael Hahn <mail@hahn-michael.com>
2019-03-29 17:33:33 +0100

    fix: typo in angular json, preventing dashboard to start

--- src/app/elasticsearch/elasticsearch.module.ts
+++ src/app/elasticsearch/elasticsearch.module.ts
@@ -1,5 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { CoreModule } from 'app/core/core.module';
+
 import { ElasticsearchComponent } from './elasticsearch.component';
 import { ElasticsearchRoutingModule } from './elasticsearch-routing.module';
 
@@ -7,6 +10,8 @@ import { ElasticsearchRoutingModule } from './elasticsearch-routing.module';
 @NgModule({
   imports: [
     CommonModule,
+    FormsModule,
+    CoreModule,
     ElasticsearchRoutingModule
   ],
   declarations: [ElasticsearchComponent]
