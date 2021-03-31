b7172cb7828d13ce9cb1ee403af1bae79f71a36d
jheyl <jheyl@evoila.de>
2021-03-31 11:25:58 +0200

    fix: make bucket field uneditable if endpoint exists; changed payload of validate request

--- src/app/shared/backup/file-endpoint/file-endpoint.component.ts
+++ src/app/shared/backup/file-endpoint/file-endpoint.component.ts
@@ -1,104 +1,128 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
-import { BackupService } from '../backup.service';
-import { ActivatedRoute, Router } from '@angular/router';
-import { NotificationService, Notification, NotificationType } from '../../../core/notification.service';
-import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
-import { BackupPlan } from '../domain/backup-plan';
+import { Component, ElementRef, OnInit, ViewChild } from "@angular/core";
+import { BackupService } from "../backup.service";
+import { ActivatedRoute, Router } from "@angular/router";
+import {
+  NotificationService,
+  Notification,
+  NotificationType,
+} from "../../../core/notification.service";
+import { NgbModal } from "@ng-bootstrap/ng-bootstrap";
+import { BackupPlan } from "../domain/backup-plan";
 
 @Component({
-  selector: 'sb-file-endpoint',
-  templateUrl: './file-endpoint.component.html',
-  styleUrls: ['./file-endpoint.component.scss']
+  selector: "sb-file-endpoint",
+  templateUrl: "./file-endpoint.component.html",
+  styleUrls: ["./file-endpoint.component.scss"],
 })
 export class FileEndpointComponent implements OnInit {
-  readonly ENTITY: string = 'fileDestinations';
-  destinationTypes = ['S3']; // 'SWIFT'
+  readonly ENTITY: string = "fileDestinations";
+  destinationTypes = ["S3"]; // 'SWIFT'
   // https://docs.aws.amazon.com/de_de/general/latest/gr/rande.html#s3_region
-  regions = [{
-    key: 'USA East (Ohio)',
-    value: 'us-east-2'
-  },{
-    key: 'USA East (North-Virginia)',
-    value: 'us-east-1'
-  },{
-    key: 'USA West (North-California)',
-    value: 'us-east-2'
-  },{
-    key: 'USA West (Oregon)',
-    value: 'us-east-2'
-  },{
-    key: 'Canada (Central)',
-    value: 'ca-central-1'
-  },{
-    key: 'Asia-Pacific (Mumbai)',
-    value: 'ap-south-1'
-  },{
-    key: 'Asia-Pacific (Seoul)',
-    value: 'ap-northeast-2'
-  },{
-    key: 'Asia-Pacific (Osaka-Local)',
-    value: 'ap-northeast-3'
-  },{
-    key: 'Asia-Pacific (Singapur)',
-    value: 'ap-southeast-1'
-  },{
-    key: 'Asia-Pacific (Sydney)',
-    value: 'ap-southeast-2'
-  },{
-    key: 'Asia-Pacific (Tokio)',
-    value: 'ap-northeast-1'
-  },{
-    key: 'China (Peking)',
-    value: 'cn-north-1'
-  },{
-    key: 'China (Ningxia)',
-    value: 'cn-northwest-1'
-  },{
-    key: 'EU (Frankfurt)',
-    value: 'eu-central-1'
-  },{
-    key: 'EU (Irland)',
-    value: 'eu-west-1'
-  },{
-    key: 'EU (London)',
-    value: 'eu-west-2'
-  },{
-    key: 'EU (Paris)',
-    value: 'eu-west-3'
-  },{
-    key: 'South-America (São Paulo)',
-    value: 'sa-east-1'
-  }];
+  regions = [
+    {
+      key: "USA East (Ohio)",
+      value: "us-east-2",
+    },
+    {
+      key: "USA East (North-Virginia)",
+      value: "us-east-1",
+    },
+    {
+      key: "USA West (North-California)",
+      value: "us-east-2",
+    },
+    {
+      key: "USA West (Oregon)",
+      value: "us-east-2",
+    },
+    {
+      key: "Canada (Central)",
+      value: "ca-central-1",
+    },
+    {
+      key: "Asia-Pacific (Mumbai)",
+      value: "ap-south-1",
+    },
+    {
+      key: "Asia-Pacific (Seoul)",
+      value: "ap-northeast-2",
+    },
+    {
+      key: "Asia-Pacific (Osaka-Local)",
+      value: "ap-northeast-3",
+    },
+    {
+      key: "Asia-Pacific (Singapur)",
+      value: "ap-southeast-1",
+    },
+    {
+      key: "Asia-Pacific (Sydney)",
+      value: "ap-southeast-2",
+    },
+    {
+      key: "Asia-Pacific (Tokio)",
+      value: "ap-northeast-1",
+    },
+    {
+      key: "China (Peking)",
+      value: "cn-north-1",
+    },
+    {
+      key: "China (Ningxia)",
+      value: "cn-northwest-1",
+    },
+    {
+      key: "EU (Frankfurt)",
+      value: "eu-central-1",
+    },
+    {
+      key: "EU (Irland)",
+      value: "eu-west-1",
+    },
+    {
+      key: "EU (London)",
+      value: "eu-west-2",
+    },
+    {
+      key: "EU (Paris)",
+      value: "eu-west-3",
+    },
+    {
+      key: "South-America (São Paulo)",
+      value: "sa-east-1",
+    },
+  ];
   destination: any = {
-    type: 'S3'
+    type: "S3",
   };
   update = false;
   validated = false;
-  submitLabel = 'Validate';
+  submitLabel = "Validate";
   submitButtonLocked = false;
 
-  @ViewChild('notdeletablecontent')
+  @ViewChild("notdeletablecontent")
   not_deletable_modal: ElementRef;
   backupPlans: BackupPlan[];
 
-  constructor(protected readonly backupService: BackupService,
+  constructor(
+    protected readonly backupService: BackupService,
     protected readonly route: ActivatedRoute,
     protected readonly router: Router,
     protected readonly nService: NotificationService,
-    protected readonly modalService: NgbModal) { }
+    protected readonly modalService: NgbModal
+  ) {}
 
   ngOnInit() {
-    this.route.params.subscribe(params => {
-      console.log(params['fileEndpointId']);
-      if (params['fileEndpointId'] && (params['fileEndpointId'] != 'new')) {
-        this.update = true;        
-        this.backupService.loadOne(this.ENTITY, params['fileEndpointId']) 
-          .subscribe(
-            (destination: any) => { 
-              this.destination = destination;
-              console.log(destination);
-            },
-          );    
+    this.route.params.subscribe((params) => {
+      console.log(params["fileEndpointId"]);
+      if (params["fileEndpointId"] && params["fileEndpointId"] != "new") {
+        this.update = true;
+        this.backupService
+          .loadOne(this.ENTITY, params["fileEndpointId"])
+          .subscribe((destination: any) => {
+            this.destination = destination;
+            console.log(destination);
+          });
         // need to load als plans as well
         // for the case the user wants to delete the fileendpoint this controller is about
         // we have to check if this fileendpoint is used by a plan. This works best if the list of plans is alredy in memory
@@ -107,121 +131,133 @@ export class FileEndpointComponent implements OnInit {
           .loadAll("backupPlans", 0) // passing a zero as pagination value, which is not really tested in all cenarios
           .subscribe((backupPlans: any) => {
             this.backupPlans = backupPlans.content;
-        });
-
+          });
       }
     });
   }
 
   delete(content): void {
-
     console.log("attempting to delete fileendpoint");
-    
-    if(this.endpointIsUsedByAnyPlan()){
-      this.modalService.open(this.not_deletable_modal, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
-          console.log("user has been informed that endpoint deletion is not possible")
-      })
-    }
-    else{
-      this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
-      this.backupService.deleteOne(this.ENTITY, this.destination)
-    .subscribe((plan: any) => {
-      console.log("fileendpoint deletion response:");
-      console.log(plan);
-      this.redirect();
-    });
-    
-    
-  }, (reason) => {
-    // we do nothing here, because user does not want to delete entity
-  });
-    }
 
+    if (this.endpointIsUsedByAnyPlan()) {
+      this.modalService
+        .open(this.not_deletable_modal, { ariaLabelledBy: "modal-basic-title" })
+        .result.then((result) => {
+          console.log(
+            "user has been informed that endpoint deletion is not possible"
+          );
+        });
+    } else {
+      this.modalService
+        .open(content, { ariaLabelledBy: "modal-basic-title" })
+        .result.then(
+          (result) => {
+            this.backupService
+              .deleteOne(this.ENTITY, this.destination)
+              .subscribe((plan: any) => {
+                console.log("fileendpoint deletion response:");
+                console.log(plan);
+                this.redirect();
+              });
+          },
+          (reason) => {
+            // we do nothing here, because user does not want to delete entity
+          }
+        );
+    }
   }
 
-
-  endpointIsUsedByAnyPlan(): boolean{
+  endpointIsUsedByAnyPlan(): boolean {
     let veto = false;
-    this.backupPlans.forEach(plan => {
-      const planDestID = plan['fileDestination']['id'];
-      if (planDestID === (this.destination.id)){
+    this.backupPlans.forEach((plan) => {
+      const planDestID = plan["fileDestination"]["id"];
+      if (planDestID === this.destination.id) {
         veto = true;
         return;
       }
-      
     });
     return veto;
   }
 
   check_endpoint_protocol(destination): boolean {
-    
-    if (destination['endpoint']){
-      if (destination['endpoint'].length == 0){
+    if (destination["endpoint"]) {
+      if (destination["endpoint"].length == 0) {
         // no endpoint set
         return false;
-      }else if (destination['endpoint'].includes('http://') || destination['endpoint'].includes('https://')){
+      } else if (
+        destination["endpoint"].includes("http://") ||
+        destination["endpoint"].includes("https://")
+      ) {
         // endpoint with at least one http or https set
         return true;
       }
-       // endpoint without protocol set
+      // endpoint without protocol set
       return false;
     }
     return false;
-    }
-    
-  onSubmit(): void {
+  }
 
-    if (this.submitButtonLocked){
+  onSubmit(): void {
+    if (this.submitButtonLocked) {
       return;
     }
 
     // actively disbale validate/submit button as long as request has led to a response
     this.submitButtonLocked = true;
-    this.submitLabel = 'Validating..';
+    this.submitLabel = "Validating..";
 
     if (!this.validated) {
       /* VALIDATE */
       // assure optional region value is at least an empty string if unset
-      if (!this.destination['region']){
-          this.destination['region'] = "";
+      if (!this.destination["region"]) {
+        this.destination["region"] = "";
       }
-      
+
       // valite endpoint contains https:// or http://
-      if (!this.check_endpoint_protocol(this.destination)){
-        this.nService.add(new Notification(NotificationType.Warning, 'Please set an endpoint beginning with http:// or https://'));
+      if (!this.check_endpoint_protocol(this.destination)) {
+        this.nService.add(
+          new Notification(
+            NotificationType.Warning,
+            "Please set an endpoint beginning with http:// or https://"
+          )
+        );
         this.submitButtonLocked = false;
-        this.submitLabel = 'Validate';
-        return
+        this.submitLabel = "Validate";
+        return;
       }
-      this.backupService.validate(this.ENTITY, this.destination)
-      
+      this.destination.serviceInstance = this.backupService.getServiceInstance();
+      this.backupService
+        .validate(this.ENTITY, this.destination)
+
         .subscribe({
           next: (d) => {
             this.submitButtonLocked = false;
             this.validated = true;
-            this.submitLabel = 'Submit';
+            this.submitLabel = "Submit";
           },
           error: (e) => {
             this.submitButtonLocked = false;
-            this.submitLabel = 'Validate';
-            this.nService.add(new Notification(NotificationType.Warning, e.error));
-          }
+            this.submitLabel = "Validate";
+            this.nService.add(
+              new Notification(NotificationType.Warning, e.error)
+            );
+          },
         });
     } else {
       /* SUBMIT */
-      this.submitLabel = 'Submitting';
+      this.submitLabel = "Submitting";
       const id = this.update ? this.destination.id : null;
       this.destination.serviceInstance = this.backupService.getServiceInstance();
-      this.backupService.saveOne(this.destination, this.ENTITY, id)
+      this.backupService
+        .saveOne(this.destination, this.ENTITY, id)
         .subscribe((destination: any) => {
-          this.submitLabel = 'Success';
+          this.submitLabel = "Success";
           this.redirect();
         });
     }
   }
 
   private redirect(): void {
-    this.router.navigate(['/backup/file-endpoints']);
+    this.router.navigate(["/backup/file-endpoints"]);
   }
-
 }
