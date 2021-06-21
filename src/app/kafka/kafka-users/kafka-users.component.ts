014e5f344baf640b5f38f8476b3b013d97621760
Moritz Mahler <moritzmahler45@gmail.com>
2021-06-21 12:15:21 +0200

    fix: better layout for kafka user form

--- src/app/kafka/kafka-users/kafka-users.component.ts
+++ src/app/kafka/kafka-users/kafka-users.component.ts
@@ -17,75 +17,104 @@ export class KafkaUsersComponent implements OnInit {
         {
           "type": "div",
           "displayFlex": true,
-          "flex-direction": "row",
+          "flex-direction": "column",
           "items": [
-            { "type": "text",
-              "key": "users[].username",
-              "flex": "4 4 200px",
-              "notitle": false,
-              "placeholder": "Username"
-            },
+
+
             {
-              "type": "password",
-              "key": "users[].password",
-              "flex": "4 4 200px",
-              "notitle": false,
-              "placeholder": "Password"
-            },
-            {
-              "type" : "array",
-              "key" : "users[].topic_acls",
-              "items" : [
-                { "type": "text",
-                  "key": "users[].topic_acls[].topic",
-                  "flex": "4 4 200px",
-                  "notitle": false,
-                  "placeholder": "Topic"
-                },
-                {
-                  "type": "text",
-                  "key": "users[].topic_acls[].rights",
-                  "flex": "4 4 200px",
-                  "notitle": false,
-                  "placeholder": "Alter, AlterConfigs, Create, Delete, Describe, DescribeConfigs, Read, Write"
-                }
-              ]
-            },
-            {
-              "type" : "array",
-              "key" : "users[].group_acls",
-              "items" : [
-                { "type": "text",
-                  "key": "users[].group_acls[].group",
-                  "flex": "4 4 200px",
-                  "notitle": false,
-                  "placeholder": "Group"
-                },
-                {
-                  "type": "text",
-                  "key": "users[].group_acls[].rights",
-                  "flex": "4 4 200px",
-                  "notitle": false,
-                  "placeholder": "Delete, Describe, Read"
-                }
-              ]
-            },
-            {
-              "type" : "array",
-              "key" : "users[].cluster_acls",
-              "maxItems" : 1,
-              "items" : [
-                {
-                  "type": "text",
-                  "key": "users[].cluster_acls[].rights",
-                  "flex": "4 4 200px",
-                  "notitle": false,
-                  "placeholder": "Alter, AlterConfigs, ClusterAction, Create, Describe, DescribeConfigs, IdempotentWrite"
-                }
-              ]
-            }
-          ]
-        }
+              "type": "div",
+              "displayFlex": true,
+              "flex-direction": "row",
+              "items": [
+
+
+
+                  { "type": "text",
+                    "key": "users[].username",
+                    "flex": "4 4 200px",
+                    "notitle": false,
+                    "placeholder": "Username"
+                  },
+                  {
+                    "type": "password",
+                    "key": "users[].password",
+                    "flex": "4 4 200px",
+                    "notitle": false,
+                    "placeholder": "Password"
+                  }
+                  
+                ]
+              },
+
+
+          {
+            "type": "div",
+            "displayFlex": true,
+            "flex-direction": "row",
+            "items": [
+
+              {
+                "type" : "array",
+                "key" : "users[].topic_acls",
+                "items" : [
+                  { "type": "text",
+                    "key": "users[].topic_acls[].topic",
+                    "flex": "4 4 200px",
+                    "notitle": false,
+                    "placeholder": "Topic"
+                  },
+                  {
+                    "type": "text",
+                    "key": "users[].topic_acls[].rights",
+                    "flex": "4 4 200px",
+                    "notitle": false,
+                    "placeholder": "Alter, AlterConfigs, Create, Delete, Describe, DescribeConfigs, Read, Write"
+                  }
+                ]
+              },
+              {
+                "type" : "array",
+                "key" : "users[].group_acls",
+                "items" : [
+                  { "type": "text",
+                    "key": "users[].group_acls[].group",
+                    "flex": "4 4 200px",
+                    "notitle": false,
+                    "placeholder": "Group"
+                  },
+                  {
+                    "type": "text",
+                    "key": "users[].group_acls[].rights",
+                    "flex": "4 4 200px",
+                    "notitle": false,
+                    "placeholder": "Delete, Describe, Read"
+                  }
+                ]
+              },
+              {
+                "type" : "array",
+                "key" : "users[].cluster_acls",
+                "maxItems" : 1,
+                "items" : [
+                  {
+                    "type": "text",
+                    "key": "users[].cluster_acls[].rights",
+                    "flex": "4 4 200px",
+                    "notitle": false,
+                    "placeholder": "Alter, AlterConfigs, ClusterAction, Create, Describe, DescribeConfigs, IdempotentWrite"
+                  }
+                ]
+              }
+
+            ]
+          
+          }
+
+        ]
+      }
+            
+          
+        
       ]
     }
   ];
--- src/environments/runtime-environment.ts
+++ src/environments/runtime-environment.ts
@@ -10,14 +10,14 @@ if (!injectedEnv) {
     "DASHBOARD DEFAULT DEVELOPMENT ENV ACTIVE - DID YOU FORGET TO INJECT A RUNTIME ENV?"
   );
   seedEnv = {
-    serviceInstanceId: "fd38b550-517d-4022-83a7-12abaf1b4124",
+    serviceInstanceId: "5706d00c-195c-40da-8a71-07198fc956e0",
     token:
-      "Bearer nah",
+      "Bearer eyJhbGciOiJSUzI1NiIsImprdSI6Imh0dHBzOi8vdWFhLnN5c3RlbS5jZi5ob2IubG9jYWwvdG9rZW5fa2V5cyIsImtpZCI6ImtleS0xIiwidHlwIjoiSldUIn0.eyJqdGkiOiJhNGUyYjFiYzk5ZWQ0ODk1YTIzNzk4YzBhNjY2NWUxMCIsInN1YiI6IjQ4OTljYzg5LTNlYTAtNGQ3Mi05MDQ4LWY1M2Y4N2M4YjMyMSIsInNjb3BlIjpbImNsb3VkX2NvbnRyb2xsZXJfc2VydmljZV9wZXJtaXNzaW9ucy5yZWFkIiwib3BlbmlkIl0sImNsaWVudF9pZCI6Im9zYi1rYWZrYS10ZXN0LnN5c3RlbS5jZi5ob2IubG9jYWwiLCJjaWQiOiJvc2Ita2Fma2EtdGVzdC5zeXN0ZW0uY2YuaG9iLmxvY2FsIiwiYXpwIjoib3NiLWthZmthLXRlc3Quc3lzdGVtLmNmLmhvYi5sb2NhbCIsImdyYW50X3R5cGUiOiJhdXRob3JpemF0aW9uX2NvZGUiLCJ1c2VyX2lkIjoiNDg5OWNjODktM2VhMC00ZDcyLTkwNDgtZjUzZjg3YzhiMzIxIiwib3JpZ2luIjoiRXZvaWxhU1NPIiwidXNlcl9uYW1lIjoibW1haGxlckBldm9pbGEuZGUiLCJlbWFpbCI6Im1tYWhsZXJAZXZvaWxhLmRlIiwiYXV0aF90aW1lIjoxNjI0MjY3NTkzLCJyZXZfc2lnIjoiMThiNDAxODUiLCJpYXQiOjE2MjQyNjc1OTMsImV4cCI6MTYyNDMxMDc5MywiaXNzIjoiaHR0cHM6Ly91YWEuc3lzdGVtLmNmLmhvYi5sb2NhbC9vYXV0aC90b2tlbiIsInppZCI6InVhYSIsImF1ZCI6WyJvcGVuaWQiLCJvc2Ita2Fma2EtdGVzdC5zeXN0ZW0uY2YuaG9iLmxvY2FsIiwiY2xvdWRfY29udHJvbGxlcl9zZXJ2aWNlX3Blcm1pc3Npb25zIl19.rcfpeVx3XxXrQqskIkipGoCQ4X4ppe4OeVG5xA6wPwd0mP6_Gf2aON7AVEFPxJmMJ-h0EwFnrvfjv4jp1Kuh627w3GTLQKEA3Cu1hF2Wpki1wCqfMO0nBCdeoTkiQBVU0f1IzoddFeLNRRFkqyIcqtJ04g_oxFCmh6F1tvimhskanU8PJ091PtfAb3g_2vKJi3ytnZKhS6-RAf5stJNk12erRng8Mhr7lKIXLUtPz-EirpjrwaGufFw9M2-KcP2oB7XOg9upBxSmMVwxoA2ylAhtT6DpxH3C-0VQq4IOiSeQzX9_WhFzDMGeTBIU1f_oMRbUmgMmf_oqXgVDThFkrw",
     baseUrls: {
       serviceBrokerUrl: "https://osb-kafka-test.system.cf.hob.local",
     },
     customEndpoints: [
-      { url: "http://localhost:8090", identifier: "log-metric-backend" },
+      { url: "https://osb-kafka-test.system.cf.hob.local", identifier: "log-metric-backend" },
       {
         url: "https://osb-backup-manager-test.system.cf.hob.local",
         identifier: "osb-backup-manager",
